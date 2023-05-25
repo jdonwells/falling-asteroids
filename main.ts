f49c89b167d68bf1f27a60b63b6f226fa6dbf3d2
Don Wells <jdonwells@gmail.com>
2023-05-25 10:48:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,85 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 . . . . . . . 
+        . . . . . 2 3 1 3 2 . . . . . . 
+        . . . . . 3 1 1 1 3 . . . . . . 
+        . . . . . 3 1 1 1 3 . . . . . . 
+        . . . . . 3 1 1 1 3 . . . . . . 
+        . . . . . 3 1 1 1 2 . . . . . . 
+        . . . . . 2 1 1 1 2 . . . . . . 
+        . . . . . 2 3 1 3 2 . . . . . . 
+        . . . . . . 3 1 3 . . . . . . . 
+        . . . . . . 2 1 2 . . . . . . . 
+        . . . . . . 2 1 2 . . . . . . . 
+        . . . . . . 2 1 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -50)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.setVelocity(0 - sprite.vx, 0 - sprite.vy)
+    otherSprite.setVelocity(0 - otherSprite.vx, 0 - otherSprite.vy)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprites.destroy(sprite, effects.disintegrate, 500)
+    info.changeScoreBy(1)
+    music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.UntilDone)
+    scene.cameraShake(2, 200)
+})
+let asteroid: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 7 . . . . . . . 
+    . . . . . . 8 8 5 6 . . . . . . 
+    . . . . . . 8 7 5 6 . . . . . . 
+    . . . . . c c c 6 6 6 . . . . . 
+    . . . . 8 8 7 7 7 5 6 6 . . . . 
+    . . 8 f f f c c 6 6 f f 6 6 . . 
+    . 8 8 8 8 6 6 7 7 7 7 5 7 6 6 . 
+    8 8 8 8 8 8 6 6 7 7 7 5 7 7 6 6 
+    8 8 8 8 8 8 6 6 7 7 7 7 5 7 6 6 
+    `, SpriteKind.Player)
+mySprite.setPosition(75, 111)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    asteroid = sprites.createProjectileFromSide(img`
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
+        `, randint(-50, 50), 50)
+    asteroid.setKind(SpriteKind.Enemy)
+    asteroid.x = randint(0, 160)
+    asteroid.setFlag(SpriteFlag.BounceOnWall, true)
+})
